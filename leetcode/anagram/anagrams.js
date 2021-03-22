f440b35f920bf2cc1203c895923256a34b4ed4ec
Ryan Evanyo <ryan.evanyo@gmail.com>
2021-03-22 15:37:17 -0600

    Anagrams

---
+++ leetcode/anagram/anagrams.js
@@ -0,0 +1,14 @@
+// Checks words are anagrams ie silent and listen are anagrams
+
+const anagrams = (s1, s2) => {
+  let sorted1 = s1.split("").sort().join("");
+  let sorted2 = s2.split("").sort().join("");
+
+  if (sorted1 === sorted2) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log();
